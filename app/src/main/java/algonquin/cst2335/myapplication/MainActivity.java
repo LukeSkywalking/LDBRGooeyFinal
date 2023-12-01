ea0080b674ce12dca9a86c4844758077b14dc573
LBRoy <144073433+LBRoy@users.noreply.github.com>
2023-12-01 18:42:39 -0500

    The Beginning

--- app/src/main/java/algonquin/cst2335/myapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/myapplication/MainActivity.java
@@ -2,13 +2,21 @@ package algonquin.cst2335.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import algonquin.cst2335.myapplication.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.recipeButton.setOnClickListener(click ->
+                startActivity(new Intent(this,.class)));
     }
 }
